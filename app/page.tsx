6f9786f9157d67d5fda5ed2e8b2ccabbaca3ca05
rajyavardhanbithale <bithale03@gmail.com>
2024-08-31 03:09:12 +0530

    responsive 4

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,19 @@ import Recommended from "./components/Recommended";
 export default function Home() {
 
   return (
-    <main className="flex flex-col gap-10 mb-20 sm:max-w-[768px] sm:mt-20 sm:m-20">
-      <Header />
-      <RatingCard />
-      <div className="block sm:fixed sm:right-0">
-        <Appointment />
-      </div>
-      <Recommended />
-      <StickyBook />
-    </main>
+    <>
+      <main className="flex flex-col gap-10 mb-20 sm:max-w-[768px] sm:mt-20 sm:m-20 sm:mx-auto xl:mx-20">
+        <Header />
+        <RatingCard />
+
+        <div className="block xl:fixed lg:right-10">
+          <Appointment />
+        </div>
+
+        <Recommended />
+        <StickyBook />
+      </main>
+
+    </>
   );
 }
